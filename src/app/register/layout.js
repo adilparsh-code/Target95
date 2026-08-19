e1e22d445e0f77c8b473ad0c01458571486ce5d2
adilparsh-code <adil.parsh@gmail.com>
2026-08-19 22:52:13 +0530

    seo: add registration metadata

---
+++ src/app/register/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Create Account | Target95+",
+  description: "Create a Target95+ account and start preparing for ICSE and ISC Computer Science exams.",
+  robots: {
+    index: false,
+    follow: false,
+  },
+};
+
+export default function RegisterLayout({ children }) {
+  return children;
+}

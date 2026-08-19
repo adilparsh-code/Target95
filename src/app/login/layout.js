13dc1c055091e56d91388d71a569da6f732bb984
adilparsh-code <adil.parsh@gmail.com>
2026-08-19 22:52:09 +0530

    seo: add login metadata

---
+++ src/app/login/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Sign In | Target95+",
+  description: "Sign in to your Target95+ account to continue your ICSE and ISC Computer Science preparation.",
+  robots: {
+    index: false,
+    follow: false,
+  },
+};
+
+export default function LoginLayout({ children }) {
+  return children;
+}
